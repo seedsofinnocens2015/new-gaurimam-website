7c404366753cfda1a77132407cc4d8bbbc8c4f07
amit04-07-1999 <ameykumar76@gmail.com>
2026-01-10 11:51:26 +0530

    ffvf

--- src/components/SuccessStories.jsx
+++ src/components/SuccessStories.jsx
@@ -112,15 +112,13 @@ const SuccessStories = () => {
               alt={`Success Story Case ${activeCase}`}
               className="mobile-slider-image"
             />
-            <div className="mobile-text-overlay">
-              <p>{currentCase.text}</p>
-            </div>
+          </div>
+          <div className="mobile-blue-bar"></div>
+          <div className="mobile-text-overlay">
+            <p>{currentCase.text}</p>
           </div>
           <button className="mobile-nav-arrow" onClick={nextCase}>
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="12" cy="12" r="11" fill="rgba(0, 0, 0, 0.5)" stroke="white" strokeWidth="1"/>
-              <path d="M9 6L15 12L9 18" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-            </svg>
+          <svg viewBox="-19.04 0 75.804 75.804" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Group_65" data-name="Group 65" transform="translate(-831.568 -384.448)"> <path id="Path_57" data-name="Path 57" d="M833.068,460.252a1.5,1.5,0,0,1-1.061-2.561l33.557-33.56a2.53,2.53,0,0,0,0-3.564l-33.557-33.558a1.5,1.5,0,0,1,2.122-2.121l33.556,33.558a5.53,5.53,0,0,1,0,7.807l-33.557,33.56A1.5,1.5,0,0,1,833.068,460.252Z" fill="#ffffff"></path> </g> </g></svg>
           </button>
         </div>
         <div className="mobile-case-indicators">
